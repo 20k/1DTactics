f2ceaa5427b3459a8796a1b6ee859997e94985c5
James <Icedshot@gmail.com>
2019-11-04 23:23:38 +0000

    definitions

--- renderables.hpp
+++ renderables.hpp
@@ -3,6 +3,8 @@
 
 #include <vec/vec.hpp>
 #include <vector>
+#include <map>
+#include <SFML/Graphics.hpp>
 
 #define TILE_PIX 16
 #define TILE_SEP 1
@@ -46,135 +48,44 @@ namespace tiles
     };
 }
 
-template<typename T>
-void add_to(T& in, vec2i loc)
+namespace level_info
 {
-    in.push_back(loc);
+    enum types
+    {
+        BARREN, ///dirt, some grass
+        DESERT,
+        GRASS
+    };
 }
 
-std::map<tiles::types, std::vector<vec2i>>& get_locations()
+///in 1bit spritemap
+std::map<tiles::types, std::vector<vec2i>>& get_locations();
+
+struct renderable_object
 {
-    static std::map<tiles::types, std::vector<vec2i>> ret;
-
-    using namespace tiles;
-
-    ///consider adding skull and crossbones to dirt
-    add_to(ret[DIRT], {0, 0});
-    add_to(ret[DIRT], {1, 0});
-    add_to(ret[DIRT], {2, 0});
-    add_to(ret[DIRT], {3, 0});
-    add_to(ret[DIRT], {4, 0});
-
-    add_to(ret[GRASS], {0, 0});
-    add_to(ret[GRASS], {5, 0});
-    add_to(ret[GRASS], {6, 0});
-    add_to(ret[GRASS], {7, 0});
-
-    add_to(ret[TREE_1], {0, 1});
-    add_to(ret[TREE_1], {1, 1});
-    add_to(ret[TREE_2], {2, 1});
-    add_to(ret[TREE_2], {4, 1});
-    add_to(ret[TREE_DENSE], {3, 1});
-    add_to(ret[TREE_DENSE], {3, 2});
-    add_to(ret[TREE_ROUND], {5, 1});
-    add_to(ret[TREE_ROUND], {4, 2});
-
-    add_to(ret[CACTUS], {6, 1});
-    add_to(ret[CACTUS], {7, 1});
-
-    add_to(ret[VINE], {2, 2});
-    add_to(ret[SHRUB], {0, 2});
-
-    add_to(ret[BRAMBLE], {0, 2});
-    add_to(ret[BRAMBLE], {0, 6});
-
-    ///top left man is 23, 0
-
-    add_to(ret[CIVILIAN], {24, 0});
-    add_to(ret[CIVILIAN], {25, 0});
-    add_to(ret[CIVILIAN], {25, 1});
-    add_to(ret[CIVILIAN], {26, 1});
-    add_to(ret[CIVILIAN], {28, 1});
-    add_to(ret[CIVILIAN], {29, 1});
-    add_to(ret[CIVILIAN], {29, 3});
-    add_to(ret[CIVILIAN], {30, 3});
-    add_to(ret[CIVILIAN], {29, 4});
-    add_to(ret[CIVILIAN], {30, 4});
-    ///top left scorpion is 23, 5
-
-    add_to(ret[SOLDIER], {25, 4});
-    add_to(ret[SOLDIER], {29, 9});
-
-    add_to(ret[GROUND_BUG], {27, 5});
-    add_to(ret[GROUND_BUG], {28, 5});
-    add_to(ret[GROUND_BUG], {29, 5});
-    add_to(ret[SMALL_PINCHY], {30, 5});
-    add_to(ret[FLYING_BUG], {25, 5});
-    add_to(ret[ARMOURED_BUG], {26, 5});
-    add_to(ret[SCORPION], {23, 5});
-
-    add_to(ret[LAND_ANIMAL], {24, 7});
-    add_to(ret[LAND_ANIMAL], {25, 7});
-    add_to(ret[LAND_ANIMAL], {26, 7});
-    add_to(ret[LAND_ANIMAL], {27, 7});
-    //add_to(ret[LAND_ANIMAL], {28, 7});
-    add_to(ret[LAND_ANIMAL], {29, 7});
-    add_to(ret[LAND_ANIMAL], {30, 7});
-    add_to(ret[LAND_ANIMAL], {25, 8}); //bat
-
-    add_to(ret[SEA_ANIMAL], {24, 8});
-    add_to(ret[SEA_ANIMAL], {27, 8});
-    add_to(ret[CROCODILE], {28, 8});
-
-    add_to(ret[FACE_MALE], {23, 10});
-    add_to(ret[FACE_MALE], {25, 10});
-    add_to(ret[FACE_MALE], {26, 10});
-    add_to(ret[FACE_MALE], {27, 10});
-    add_to(ret[FACE_MALE], {28, 10});
-
-    add_to(ret[FACE_WOMAN], {24, 10});
-    add_to(ret[FACE_WOMAN], {29, 10});
-    add_to(ret[FACE_WOMAN], {30, 10});
-
-    add_to(ret[THIN_DOOR_CLOSED], {3, 4});
-    add_to(ret[THIN_DOOR_OPEN], {4, 4});
-
-    add_to(ret[DOOR_CLOSED], {3, 3});
-    add_to(ret[THIN_DOOR_OPEN], {4, 3});
-
-    add_to(ret[GRAVE], {0, 14});
-    add_to(ret[GRAVE], {1, 14});
-    add_to(ret[GRAVE], {2, 14});
-
-    add_to(ret[WOOD_FENCE_FULL], {1, 3});
-    add_to(ret[WOOD_FENCE_FULL], {2, 3});
-    add_to(ret[WOOD_FENCE_HALF], {0, 3});
-
-    add_to(ret[TILING_WALL], {16, 19});
-    add_to(ret[TILING_WALL], {17, 19});
-    add_to(ret[TILING_WALL], {18, 19});
-
-    add_to(ret[TILING_WALL], {16, 20});
-    add_to(ret[TILING_WALL], {17, 20});
-    add_to(ret[TILING_WALL], {18, 20});
-
-    add_to(ret[TILING_WALL], {16, 21});
-    add_to(ret[TILING_WALL], {17, 21});
-    add_to(ret[TILING_WALL], {18, 21});
-
-    add_to(ret[CULTIVATION], {13, 6});
-    add_to(ret[CULTIVATION], {14, 6});
-    add_to(ret[CULTIVATION], {15, 6});
-    add_to(ret[CULTIVATION], {16, 6});
-    add_to(ret[CULTIVATION], {17, 6});
-
-    return ret;
-}
-
-struct entity_manager
-{
-
+    vec2i tile_id = {0,0};
+    vec4f lin_colour = {1,1,1,1};
 };
 
+struct tile_object
+{
+    renderable_object obj;
+    float path_cost = 1;
+    bool passable = true;
+};
+
+struct playspace_manager
+{
+    vec2f camera;
+    float zoom = 1;
+
+    vec2i level_size = {0,0};
+    std::vector<tile_object> all_tiles;
+
+    void create_level(vec2i dim, level_info::types type);
+
+    void tick(double dt_s);
+    void draw(sf::RenderWindow& win);
+};
 
 #endif // RENDERABLES_HPP_INCLUDED
