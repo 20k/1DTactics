9ee39eeb2ecb87a95a0bf1fa07df1ab04c612584
James <Icedshot@gmail.com>
2019-11-22 08:05:54 +0000

    fix crash on shooting self

--- combat_model.cpp
+++ combat_model.cpp
@@ -56,13 +56,15 @@ float body_part::get_functionality()
 ///crits?
 combat_info::hit_types body_part::roll_to_hit(std::minstd_rand& rng, float probability_modification) const
 {
-    bool direct_hit = rand_det_s(rng, 0.f, 1.f) < (base_hit_probability * probability_modification);
+    float to_hit_prob = clamp(base_hit_probability + probability_modification, 0.f, 1.f);
+
+    bool direct_hit = rand_det_s(rng, 0.f, 1.f) < to_hit_prob;
 
     bool graze_hit = false;
 
     if(!direct_hit)
     {
-        graze_hit = rand_det_s(rng, 0.f, 1.f) < (base_hit_probability * probability_modification);
+        graze_hit = rand_det_s(rng, 0.f, 1.f) < to_hit_prob;
     }
 
     if(direct_hit)
@@ -81,6 +83,7 @@ void body_part::hit(float damage)
     hp = clamp(hp, 0, max_hp);
 }
 
+///shooting self is causing a crash
 combat_info::hit_types creature_model::hit_random_bodypart_for(float damage, float probability_modification)
 {
     ///uuh this is shit
@@ -90,11 +93,19 @@ combat_info::hit_types creature_model::hit_random_bodypart_for(float damage, flo
 
     for(auto& i : parts)
     {
-        probabilities.push_back(i.base_hit_probability * probability_modification);
+        probabilities.push_back(i.base_hit_probability + probability_modification);
+    }
+
+    for(auto& i : probabilities)
+    {
+        i = clamp(i, 0.f, 1.f);
     }
 
     ///weights towards most hittable
     int random_part = random_select_with_weights(rng, probabilities);
+
+    if(random_part == -1)
+        return combat_info::MISS;
 
     combat_info::hit_types ret = parts[random_part].roll_to_hit(rng, probability_modification);
 
@@ -149,18 +160,17 @@ creature_model default_alien_model()
 
     body_part leg;
     leg.type = body_part_info::LEG;
-    leg.base_hit_probability = 0.6;
+    leg.base_hit_probability = -0.4;
 
     body_part head;
     head.type = body_part_info::HEAD;
-    head.base_hit_probability = 0.3;
-
+    head.base_hit_probability = -0.7;
 
     body_part torso;
     torso.max_hp = 4;
     torso.hp = 4;
     torso.type = body_part_info::TORSO;
-    torso.base_hit_probability = 0.8;
+    torso.base_hit_probability = -0.1;
 
     mod.parts.push_back(head);
 
@@ -182,23 +192,23 @@ creature_model default_trooper_model()
 
     body_part leg;
     leg.type = body_part_info::LEG;
-    leg.base_hit_probability = 0.6;
+    leg.base_hit_probability = -0.2;
 
     body_part arm;
     arm.type = body_part_info::ARM;
-    arm.base_hit_probability = 0.5;
+    arm.base_hit_probability = -0.5;
 
     body_part head;
     head.hp = 0.5;
     head.max_hp = 0.5;
     head.type = body_part_info::HEAD;
-    head.base_hit_probability = 0.1;
+    head.base_hit_probability = -0.7;
 
     body_part torso;
     torso.max_hp = 2;
     torso.hp = 2;
     torso.type = body_part_info::TORSO;
-    torso.base_hit_probability = 0.8;
+    torso.base_hit_probability = -0.1;
 
     mod.parts.push_back(head);
     mod.parts.push_back(torso);
@@ -290,7 +300,7 @@ float get_full_hit_probability(const entity_object& source, const entity_object&
         accuracy_mod = facet_val.value().value;
     }
 
-    return get_terrainwise_hit_probability(source, target, range) + accuracy_mod;
+    return clamp(get_terrainwise_hit_probability(source, target, range) + accuracy_mod, 0, 1);
 }
 
 void handle_attack(entity_object& source, entity_object& target, item& with)
